42a74ef0a5c52b1bdaa37287a69a6c5f8799c421
Emily Vanderveer <eviebatchild@gmail.com>
2021-07-16 19:50:30 -0400

    Update arduino-canbus-monitor.ino

--- arduino-canbus-monitor/arduino-canbus-monitor.ino
+++ arduino-canbus-monitor/arduino-canbus-monitor.ino
@@ -27,15 +27,6 @@
   mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
 #endif
 
-void setup() {
-	  Serial.begin(LW232_DEFAULT_BAUD_RATE); // default COM baud rate is 115200. 
-
-    Can232::init(CAN_500KBPS, MCP_8MHz); // set default rate you need here and clock frequency of CAN shield. Typically it is 16MHz, but on some MCP2515 + TJA1050 it is 8Mhz
-
-    // optional custom packet filter to reduce number of messages comingh through to canhacker
-     Can232::setFilter(myCustomAddressFilter); 
-}
-
 INT8U myCustomAddressFilter(INT32U addr) {
     INT8U ret = LW232_FILTER_SKIP; //LW232_FILTER_PROCESS or LW232_FILTER_SKIP
     switch(addr) {
@@ -51,6 +42,16 @@ INT8U myCustomAddressFilter(INT32U addr) {
     }
   return ret;
 }
+
+void setup() {
+	  Serial.begin(LW232_DEFAULT_BAUD_RATE); // default COM baud rate is 115200. 
+
+    Can232::init(CAN_500KBPS, MCP_8MHz); // set default rate you need here and clock frequency of CAN shield. Typically it is 16MHz, but on some MCP2515 + TJA1050 it is 8Mhz
+
+    // optional custom packet filter to reduce number of messages comingh through to canhacker
+     Can232::setFilter(myCustomAddressFilter); 
+}
+
 
 void loop() {
     Can232::loop();
